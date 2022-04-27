e80f89b4dca1eadb5993bab1a5af636abbec40a5
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-04-27 15:57:30 -0300

    27/04/22

--- React/curso_Hora_de_Codar/src/components/Form.js
+++ React/curso_Hora_de_Codar/src/components/Form.js
@@ -1,19 +1,40 @@
+import { useState } from "react";
+
 function Form () {
 
-function cadastrarUser() {
+function cadastrarUser(e) {
+e.preventDefault();
+console.log(name);
+console.log(password);
 console.log("Usuario Cadastrado!!!")
 }
 
 
 
-
+const [name, setName] = useState();
+const [password, setPassword] = useState();
 
 return(
 <div>
 <h1> Meu Cadastro</h1>
 <form onSubmit={cadastrarUser}>
 <div>
-<input type="text" placeholder="Digite o Seu Nome"/>
+<label htmlFor="name">Nome: </label>
+<input type="text" 
+id="name" 
+name="name" 
+placeholder="Digite o Seu Nome"
+onChange={(e) => setName(e.target.value)}/>
+</div>
+<div>
+    <label htmlFor="password">Senha: </label>
+<input 
+type="password" 
+id="password" 
+name="password" 
+placeholder="Digite a Senha"
+onChange={(e) => setPassword(e.target.value)}/>
+
 </div>
 <div>
 <input type="submit" value="Cadastrar"/>    
