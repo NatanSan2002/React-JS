7468553b5a1c14f91c5c744945464e62377ee694
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-05-06 12:50:14 -0300

    06/05/22 (01)

--- React/curso_Hora_de_Codar/src/components/layout/Footer.js
+++ React/curso_Hora_de_Codar/src/components/layout/Footer.js
@@ -1,7 +1,24 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from "react-icons/fa";
+
+import {DiAndroid} from "react-icons/di";
+
+import style from "./Footer.module.css";
+
+
 function Footer() {
 return (
 <>
-<footer>Rodapé</footer>
+<footer> 
+    <ul className={style.social_list}>
+    <li> <FaFacebook/> </li>
+    <li> <FaInstagram/> </li>
+    <li> <FaLinkedin/> </li>
+    <li> <DiAndroid/></li>
+
+    </ul>
+    
+<p> Nosso Rodapé</p>
+    </footer>
 </>
 )
 }
