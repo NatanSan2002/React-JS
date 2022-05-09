3bddb9a0497b34910a902ec2ebbec13a3a0ef768
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-05-09 15:00:27 -0300

    09/05/22

--- React/Hora_de_Codar_Projeto/projeto_hc/src/App.js
+++ React/Hora_de_Codar_Projeto/projeto_hc/src/App.js
@@ -1,26 +1,39 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+/* Routes = Switch*/ 
 
-/* Routes = Switch*/
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+
+import Container from "./components/pages/layout/Container.js";
+
+
 
 function App() {
   return (  
 <Router>
-<ul>
-  <li>Home</li>
-  <li>Contato</li>
-</ul>
+<div>
+  <Link to="/">Home</Link>
+  <Link to="/contact">Contact</Link>
+  <Link to="/company">Company</Link>
+  <Link to="/newproject">Newproject</Link>
+  </div>
+ 
+ <Container customClass="min_height">
 <Routes>
 <Route exact path="/" element={<Home/>}/>
 
 <Route exact path="/company" element={<Company/>}/>
 
-<Route exact path="/contato" element={<Contact/>}/>
+<Route exact path="/contact" element={<Contact/>}/>
 
 <Route exact path="/newproject" element={<NewProject/>}/>
 </Routes>
+</Container>
 
 
-<footer></footer>
+<p>Footer</p>
 
 </Router>
     
