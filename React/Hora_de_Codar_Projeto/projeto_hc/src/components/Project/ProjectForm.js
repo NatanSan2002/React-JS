7dec7b629687bcba3c385d762669cfa9cdc64ddc
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-05-16 16:36:00 -0300

    16/05/22

--- React/Hora_de_Codar_Projeto/projeto_hc/src/components/Project/ProjectForm.js
+++ React/Hora_de_Codar_Projeto/projeto_hc/src/components/Project/ProjectForm.js
@@ -1,9 +1,29 @@
+import {useState} from "react";
+
 import Input from "../form/input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import style from "./ProjectForm.module.css";
 
+
+
+
 function ProjectForm({btnText}) {
+
+const [categories,setCategories] = useState([]);
+
+fetch("http://localhost:5000/categories",{
+method: "GET",
+headers: {
+    "Content-Type":"application/json",
+},
+}
+)
+.then((resp) => resp.json())
+.then((data) => setCategories(data))
+.catch((error) => console.log(error) )
+
+
 return(
 <form className={style.form}>
     <Input type="text" 
@@ -16,7 +36,7 @@ return(
     name="budget" 
     placeholder="Insert The Orcamento total"/>
     
-<Select name="category_id" text="Select the Category:"/>
+<Select name="category_id" text="Select the Category:" options={categories}/>
 <SubmitButton text={btnText}/>
 </form>
 )
