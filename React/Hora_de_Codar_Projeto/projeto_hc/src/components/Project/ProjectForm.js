0615bad390c1889999fae608d2752e8359558575
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-05-23 15:13:50 -0300

    23/05/22

--- React/Hora_de_Codar_Projeto/projeto_hc/src/components/Project/ProjectForm.js
+++ React/Hora_de_Codar_Projeto/projeto_hc/src/components/Project/ProjectForm.js
@@ -31,25 +31,43 @@ useEffect(
         }, [])
 
 
-const submit = function gg (e) {
+const submit = (e) => {
 e.preventDefault();
+//console.log(project)
 handleSubmit(project);
-
 }
+
+function handleChange(e) {
+setProject({...project, [e.target.name]: e.target.value })
+}
+
+function handleCategory(e) {
+    setProject({...project, category: {
+    id: e.target.value,
+    name: e.target.options[e.target.selectedIndex].text,
+    },})
+    }
 
 return(
 <form  onSubmit={submit} className={style.form}>
     <Input type="text" 
     text="Nome do Projeto:" 
     name="name" 
-    placeholder="Insert The Name Of Project"/>
+    placeholder="Insert The Name Of Project"
+    handleOnChange={handleChange}
+    value={project.name ? project.name : ""}
+    />
   
 <Input type="number" 
     text="Orcamento of Project: " 
     name="budget" 
-    placeholder="Insert The Orcamento total"/>
+    placeholder="Insert The Orcamento total"
+    handleOnChange={handleChange}
+    value={project.budget ? project.budget : ""} />
     
-<Select name="category_id" text="Select the Category:" options={categories}/>
+<Select name="category_id" text="Select the Category:" options={categories} 
+handleOnChange={handleCategory} 
+value={project.category ? project.category.id : ""} />
 <SubmitButton text={btnText}/>
 </form>
 )
--- React/Hora_de_Codar_Projeto/projeto_hc/src/components/pages/NewProject.js
+++ React/Hora_de_Codar_Projeto/projeto_hc/src/components/pages/NewProject.js
@@ -1,17 +1,17 @@
 import style from "./NewProject.module.css";
 import ProjectForm  from "../Project/ProjectForm.js";
-import {useHistory} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 function NewProject() {
 
-    const history = useHistory();
+    const history = useNavigate();
 
     function createPost(project) {
     // initialize cost and services
     project.cost = 0;
     project.services = [];
 
-    fetch("http:/localhost:5000/projects", {
+    fetch("http://localhost:5000/projects", {
     method: "POST",
     headers:{
     "Content-type": "application/json"
@@ -19,8 +19,9 @@ function NewProject() {
     body: JSON.stringify(project),
     }).then((resp) => resp.json )
     .then(
-        (data) => console.log(data)
-        //redirect
+        (data) => {console.log(data)
+            history("/projects",{message: "Projeto Criado com Sucesso!"})}
+        
     )
     .catch(err => console.log(err));
     }
