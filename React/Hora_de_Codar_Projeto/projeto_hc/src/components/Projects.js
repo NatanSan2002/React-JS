4d9c355aa42a9e893f6358b960582144063a767b
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-05-24 15:10:27 -0300

    24/05/22

--- React/Hora_de_Codar_Projeto/projeto_hc/src/components/Projects.js
+++ React/Hora_de_Codar_Projeto/projeto_hc/src/components/Projects.js
@@ -1,9 +1,23 @@
+import Message from "./pages/layout/Message.js";
+import {useLocation} from "react-router-dom";
+
+
 function Projects() {
+
+const location = useLocation;
+let message = "";
+if(location.state){
+message = location.state.message;
+}
+
 return(
-<>
-<h1>Projects</h1>
-</>
+<div>
+<h1> My Projects</h1>
+{message && (<Message type="success" msg={message}/>)}
+
+</div>
 )
+/*Atualizar COmandos do Navigate sla oq do novo react-router-dom para funcionar*/ 
 }
 
 export default Projects; 
