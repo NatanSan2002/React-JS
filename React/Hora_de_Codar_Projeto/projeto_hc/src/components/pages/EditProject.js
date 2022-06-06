88a9d24aee5f20f892ef52d9a187ac07448698af
Natan2002 <102757932+NatanSan2002@users.noreply.github.com>
2022-06-06 16:11:54 -0300

    06/06/22

--- React/Hora_de_Codar_Projeto/projeto_hc/src/components/pages/EditProject.js
+++ React/Hora_de_Codar_Projeto/projeto_hc/src/components/pages/EditProject.js
@@ -3,10 +3,13 @@ import style from "./EditProject.module.css";
 import Input from "../form/input.js";
 import Select from "../form/Select.js";
 import SubmitButton from "../form/SubmitButton";
+import Loading from "./layout/Loading.js";
+import Container from "./layout/Container";
 
 import {useState,useEffect} from "react";
 
 import {useParams} from "react-router-dom";
+
 
 function EditProject(){
 
@@ -14,19 +17,24 @@ const {id} = useParams(); console.log(id)
 
 const [project,setProject] = useState([]);
 
+const [show,setShow] = useState(false);
+
 
 
 
 useEffect( () => {
+setTimeout( () => {
 fetch(`http://localhost:5000/projects/${id}`,{
 method: "GET",headers:{"Content-Type": "application/json"}
 }).then( (r) => r.json() )
 .then( (d) =>  {setProject(d)
 console.log(d)
-}).catch(erro => console.log(erro))},[id]
+}).catch(erro => console.log(erro))},150)},[id]
 )
 
-/*colocar efeito de loading*/
+function toggleProjectForm() {
+setShow(!show);
+}
 
 
 
@@ -34,9 +42,36 @@ console.log(d)
 
 
 return(
-<div className={style.principal}>
-<p>Projeto de id: {id}</p>
+<>
+{project.name ? (
+<div className={style.project_details}>
+<Container customClass="column">
+<div className={style.details_container}>
+<h1>Projeto: {project.name}</h1>
+<button className={style.btn} onClick={toggleProjectForm}>{
+show == false ? "Editar Projeto" : "Ocultar Projeto"}</button>
+{show == false ? (
+<div className={style.project_info}> 
+    <p>
+  <span>Categoria: </span>{project.category.name}
+    </p>
+    <p>
+    <span>Total de Orçamento: </span>R${project.budget}
+        </p>
+        <p>
+        <span>Total Utilizado: </span>R${project.cost}    
+        </p>
 </div>
+) : 
+(<div className={style.project_info}> 
+<p>Form</p>
+</div>)}
+</div>
+</Container>
+</div>
+    
+    ) : (<Loading/>)}
+</>
 
 )
 }
